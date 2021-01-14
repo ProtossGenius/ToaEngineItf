d1c01225fe194f609a61219883691f867e02d576
SureMoon <guyvejianglou@outlook.com>
2021-01-14 19:00:50 +0800

    修改AI的编写方式

--- src/com/suremoon/game/door/kernel/UnitRemItf.java
+++ src/com/suremoon/game/door/kernel/UnitRemItf.java
@@ -2,8 +2,9 @@ package com.suremoon.game.door.kernel;
 
 import com.suremoon.game.door.units_itf.UnitItf;
 
-public interface UnitRemItf extends CalcAble{
+public interface UnitRemItf {
     void underAttack(UnitItf attacker, double hurt);
+    void doCalc(UnitItf unit,  WorldItf world, WorldMgrItf worldMgr);
     UnitRemItf Null = new UnitRemNothing();
 }
 
@@ -15,7 +16,7 @@ class UnitRemNothing implements UnitRemItf{
     }
 
     @Override
-    public void doCalc(WorldItf world, WorldMgrItf worldMgr) {
+    public void doCalc(UnitItf unit,  WorldItf world, WorldMgrItf worldMgr) {
 
     }
 }

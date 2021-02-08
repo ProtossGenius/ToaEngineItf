5d1351bffb16de02b2b168c647ab9a5d6fec87a4
suremoon <protossgenius@gmail.com>
2021-02-08 22:21:13 +0800

    some comment for UnitDoItf

--- src/com/suremoon/game/door/kernel/manager/UnitDoItf.java
+++ src/com/suremoon/game/door/kernel/manager/UnitDoItf.java
@@ -3,5 +3,9 @@ package com.suremoon.game.door.kernel.manager;
 import com.suremoon.game.door.units_itf.UnitItf;
 
 public interface UnitDoItf {
+    /**
+     * @param unit
+     * @return true:break, false: continue.
+     */
     boolean Do(UnitItf unit);
 }

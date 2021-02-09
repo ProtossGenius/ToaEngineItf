f7ca3071391f575eeb46098c698bff05c1636da5
SureMoon <guyvejianglou@outlook.com>
2021-02-09 17:30:17 +0800

    修改了伤害计算,采用五行相生相克的原理.

--- src/com/suremoon/game/door/attribute/HurtCalcItf.java
+++ src/com/suremoon/game/door/attribute/HurtCalcItf.java
@@ -5,22 +5,26 @@ import com.suremoon.game.door.units_itf.UnitItf;
 public interface HurtCalcItf {
     /**
      * @param sHurt ¹¥»÷·½µÄ¹¥»÷ÊôÐÔ
-     * @return
+     * @return hurt.
      */
-    int underAttack(UnitItf unit, ElementPriorities sHurt);
+    double underAttack(UnitItf unit, ElementPriorities sHurt);
     HurtCalcItf Null = (unit, sHurt) -> {
         var attr = unit.getAttribute();
-        return oneProperty(sHurt.metal, attr.getMetal()) +
-                oneProperty(sHurt.wood, attr.getWood()) +
-                oneProperty(sHurt.water, attr.getWater()) +
-                oneProperty(sHurt.earth, attr.getEarth()) +
-                oneProperty(sHurt.thunder, attr.getThunder()) +
-                oneProperty(sHurt.water, attr.getWind()) +
-                oneProperty(sHurt.ice, attr.getIce());
+        return oneProperty(sHurt.metal, attr.getFire()) +
+                oneProperty(sHurt.wood, attr.getMetal()) +
+                oneProperty(sHurt.water, attr.getEarth()) +
+                oneProperty(sHurt.fire, attr.getWater()) +
+                oneProperty(sHurt.earth, attr.getWood()) +
+                // think thunder product by wood&water.
+                oneProperty(sHurt.thunder, attr.getMetal() * 0.5 + attr.getEarth() * 0.5) +
+                // think wind product by hot&cool.
+                oneProperty(sHurt.wind, attr.getIce() * 0.5 + attr.getFire() * 0.5) +
+                // think ice product by cool&earth.
+                oneProperty(sHurt.ice, attr.getFire() * 0.5 + attr.getWood() * 0.5);
     };
 
-    static int oneProperty(double s, double m){
+    static double oneProperty(double s, double m){
         if(m < 0)return (int)s;
-        return (int)(s - s / (m + 10));
+        return s - s / (m + 10);
     }
 }
